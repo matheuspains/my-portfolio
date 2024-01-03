64fb0e831e335b22083c132803b4d2a26b398afd
matheuspains <pains@discente.ufg.br>
2024-01-03 10:44:38 -0300

    sidebar creation

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-// Exemplo de script JavaScript - você pode personalizar conforme necessário
-console.log('Meu script JavaScript está funcionando!');
+// Aguarda o evento de carregamento completo do DOM
+document.addEventListener('DOMContentLoaded', function () {
+    
+    // Obtém a referência para o botão de alternância da barra lateral
+    const toggleSidebarButton = document.getElementById('toggleSidebar');
+    
+    // Obtém a referência para a barra lateral
+    const sidebar = document.getElementById('sidebar');
 
+    // Adiciona um ouvinte de evento para o clique no botão de alternância
+    toggleSidebarButton.addEventListener('click', function () {
+        
+        // Alterna a presença da classe 'show' na barra lateral
+        sidebar.classList.toggle('show');
+    });
+});
