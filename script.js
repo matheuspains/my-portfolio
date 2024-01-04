4f1196c0ab449d0da4355943fc70631be763b2dc
matheuspains <pains@discente.ufg.br>
2024-01-04 09:29:46 -0300

    update sidebar

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-// Aguarda o evento de carregamento completo do DOM
+// Aguarda o evento de carregamento completo do DOM (Document Object Model)
 document.addEventListener('DOMContentLoaded', function () {
-    
-    // Obtém a referência para o botão de alternância da barra lateral
-    const toggleSidebarButton = document.getElementById('toggleSidebar');
-    
-    // Obtém a referência para a barra lateral
+    // Obtém a referência para a barra lateral e o título no cabeçalho
     const sidebar = document.getElementById('sidebar');
+    const headerTitle = document.querySelector('header h1');
 
-    // Adiciona um ouvinte de evento para o clique no botão de alternância
-    toggleSidebarButton.addEventListener('click', function () {
-        
+    // Adiciona um ouvinte de evento para o clique no título no cabeçalho
+    headerTitle.addEventListener('click', function () {
         // Alterna a presença da classe 'show' na barra lateral
         sidebar.classList.toggle('show');
+
+        // Obtém as referências para os ícones à esquerda e à direita do título
+        const leftIcon = headerTitle.querySelector('span#leftSidebarIcon');
+        const rightIcon = headerTitle.querySelector('span#rightSidebarIcon');
+
+        // Alterna o conteúdo dos spans entre ">" e "<" conforme necessário
+        if (sidebar.classList.contains('show')) {
+            leftIcon.textContent = '<';
+            rightIcon.textContent = '>';
+        } else {
+            leftIcon.textContent = '>';
+            rightIcon.textContent = '<';
+        }
     });
 });
